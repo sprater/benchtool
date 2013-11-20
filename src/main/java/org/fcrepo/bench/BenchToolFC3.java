a8cda653d13bfd3e7ab0b83d97f7d6f0f8535459
Esmé Cowles <escowles@ucsd.edu>
2013-11-20 14:24:09 -0500

    Re-adding Random for selecting objects to update/delete/read in random order

--- src/main/java/org/fcrepo/bench/BenchToolFC3.java
+++ src/main/java/org/fcrepo/bench/BenchToolFC3.java
@@ -12,6 +12,7 @@ import java.net.URI;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
